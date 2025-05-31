e4c479bba69f66e69e10e4deadcb1038076b78dd
ahteshamarain <139765018+ahteshamarain@users.noreply.github.com>
2025-05-31 12:57:19 +0500

    update

--- src/slider.jsx
+++ src/slider.jsx
@@ -1,31 +1,42 @@
 import React from "react";
-
 import Carousel from 'react-bootstrap/Carousel';
 
 function SliderComponent() {
   return (
     <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400.png?text=First+Slide"
+          alt="First slide"
+        />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400.png?text=Second+Slide"
+          alt="Second slide"
+        />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400.png?text=Third+Slide"
+          alt="Third slide"
+        />
         <Carousel.Caption>
           <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
