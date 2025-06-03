758d7741cf1126f4479491373881097e92e573bc
ahteshamarain <139765018+ahteshamarain@users.noreply.github.com>
2025-06-03 11:48:41 +0500

    update

--- src/slider.jsx
+++ src/slider.jsx
@@ -7,8 +7,9 @@ function SliderComponent() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400.png?text=First+Slide"
+          src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg"
           alt="First slide"
+          style={{ height: "400px", objectFit: "cover" }}
         />
         <Carousel.Caption>
           <h3>First slide label</h3>
@@ -19,8 +20,9 @@ function SliderComponent() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400.png?text=Second+Slide"
+          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg"
           alt="Second slide"
+          style={{ height: "400px", objectFit: "cover" }}
         />
         <Carousel.Caption>
           <h3>Second slide label</h3>
@@ -31,8 +33,9 @@ function SliderComponent() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400.png?text=Third+Slide"
+          src="https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg"
           alt="Third slide"
+          style={{ height: "400px", objectFit: "cover" }}
         />
         <Carousel.Caption>
           <h3>Third slide label</h3>
