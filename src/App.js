9ca46294d6b7a716251d3f2b23ebb3f1fa1e4559
ahteshamarain <139765018+ahteshamarain@users.noreply.github.com>
2025-06-10 13:08:05 +0500

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import img3 from './images/img3.png';
 
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
   const cardsData = [
     {
@@ -19,47 +18,61 @@ function App() {
       title: 'Pushpa',
       detail: 'Pushpa The Rise.',
       link: 'https://www.imdb.com/title/tt16539454/',
+      timeRange: [9, 12], 
     },
     {
       img: img2,
       title: 'Luck',
       detail: 'Live On Your Luck.',
       link: 'https://example.com',
+      timeRange: [13, 16], 
     },
     {
       img: img3,
       title: 'Rings',
       detail: 'Suspense thriller movie.',
       link: 'https://www.pngegg.com/en/search?q=Thriller+film',
+      timeRange: [17, 20],
     },
   ];
 
+  const currentHour = new Date().getHours();
+
+
+  const filteredCards = cardsData.filter(
+    (card) => currentHour >= card.timeRange[0] && currentHour < card.timeRange[1]
+  );
+
   return (
-  <div>
-    <HeaderComponent></HeaderComponent>
-    <SliderComponent></SliderComponent>
-    <ProductComponent></ProductComponent>
-
-
+    <div>
+      <HeaderComponent />
+      <SliderComponent />
+      <ProductComponent />
 
       <Container className="my-4 py-3">
         <Row className="g-5">
-          {cardsData.map((card, index) => (
-            <Col key={index} xs={12} sm={6} md={4} lg={3}>
-              <Props
-                img={card.img}
-                title={card.title}
-                detail={card.detail}
-                link={card.link}
-              />
+          {filteredCards.length > 0 ? (
+            filteredCards.map((card, index) => (
+              <Col key={index} xs={12} sm={6} md={4} lg={3}>
+                <Props
+                  img={card.img}
+                  title={card.title}
+                  detail={card.detail}
+                  link={card.link}
+                />
+              </Col>
+            ))
+          ) : (
+            <Col>
+              <h5 className="text-center">No movies to show at this time.</h5>
             </Col>
-          ))}
+          )}
         </Row>
       </Container>
-      <Datec /> 
-    <FooterComponent></FooterComponent>
-     
-      </div>
+
+      <Datec />
+      <FooterComponent />
+    </div>
   );
 }
 
