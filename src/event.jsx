c26773975578e6f01c36b5f84d6be9540a51d3bc
ahteshamarain <139765018+ahteshamarain@users.noreply.github.com>
2025-06-12 12:33:56 +0500

    update

--- src/event.jsx
+++ src/event.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
-function Football(){
-    const shoot = (a,b) =>{
-        alert(b.type = a);
-    }
-    return(
-        <button onClick={(event) => shoot("goal" , event)}>Take the shoot</button>
-    )
-}
 // function Football(){
-//    function handleClick(){
-//     let bodystyle = document.body.style;
-//     if(bodystyle.backgroundColor === 'black'){
-//         bodystyle.backgroundColor = 'white';
+//     const shoot = (a,b) =>{
+//         alert(b.type = a);
 //     }
-//     else{
-//         bodystyle.backgroundColor = 'black'
-//     }
-//    }
-
 //     return(
-//         <button onClick={handleClick}>toogle light</button>
+//         <button onClick={(event) => shoot("goal" , event)}>Take the shoot</button>
 //     )
 // }
+function Football(){
+   function handleClick(){
+    let bodystyle = document.body.style;
+    if(bodystyle.backgroundColor === 'black'){
+        bodystyle.backgroundColor = 'white';
+    }
+    else{
+        bodystyle.backgroundColor = 'black'
+    }
+   }
+
+    return(
+        <button onClick={handleClick}>toogle light</button>
+    )
+}
 export default Football;
