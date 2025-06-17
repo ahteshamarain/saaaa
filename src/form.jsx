bad5dab2c1a30b86a17c09bea9850014f155ee7c
ahteshamarain <139765018+ahteshamarain@users.noreply.github.com>
2025-06-17 11:56:24 +0500

    update

--- src/form.jsx
+++ src/form.jsx
@@ -2,31 +2,46 @@ import React, { useState } from "react";
 
 function Blog() {
 
-    let [tltle, settitle] = useState('');
+    let [title, settitle] = useState('');
     let [blog, setblog] = useState('');
     let [auther, setauther] = useState('Ahtesham');
 
     return (
-       
         <form onSubmit={(event) => {
             event.preventDefault();
-            const blogdata = {tltle,blog,auther}
+            const blogdata = { title, blog, auther };
+            console.log(blogdata); // Checkout here: blog data logs correctly
         }}>
 
             <label>Enter Title</label>
-            <input type="text" required onChange={(event) => settitle(event.target.value)} value={title} name="" id="" />
-           <br />
-            <label>Enter blog</label>
-            <input type="text" required onChange={(event) => setblog(event.target.value)} value={blog} name="" id="" />
-           <br />
-            <select onChange={(event) => setauther(event.target.value)} value={auther} name="" id="">
+            <input 
+                type="text" 
+                required 
+                onChange={(event) => settitle(event.target.value)} 
+                value={title} 
+            />
+            <br />
+
+            <label>Enter Blog</label>
+            <input 
+                type="text" 
+                required 
+                onChange={(event) => setblog(event.target.value)} 
+                value={blog} 
+            />
+            <br />
+
+            <select 
+                onChange={(event) => setauther(event.target.value)} 
+                value={auther}
+            >
                 <option value="john">john</option>
                 <option value="kelvin">kelvin</option>
-               
             </select>
-            <button>submit</button>
+            <br />
+            
+            <button type="submit">Submit</button>
         </form>
-
     );
 }
 
